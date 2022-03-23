220f19508a8d114ddc0fb805de0103c79ac8bb72
Forrest Wilkins <fwilkins@imagequix.com>
2022-03-23 16:52:29 -0400

    Added rgbaToRandom function

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"image"
+	"math/rand"
 	"os"
 
+	"image/color"
 	_ "image/jpeg"
 	"image/png"
 )
@@ -22,24 +25,57 @@ func rgbaToGray(img image.Image) *image.Gray {
 	return gray
 }
 
+func rgbaToRandom(img image.Image) *image.RGBA {
+	var (
+		bounds = img.Bounds()
+		newImg = image.NewRGBA(bounds)
+	)
+
+	for x := 0; x < bounds.Max.X; x++ {
+		for y := 0; y < bounds.Max.Y; y++ {
+			var rgba = img.At(x, y)
+
+			r, g, b, a := rgba.RGBA()
+
+			var alphaPremultipliedArray = []uint32{r, g, b, a}
+
+			if r > 0 {
+				fmt.Printf("rgba: %+v\n", rgba)
+				fmt.Printf("alphaPremultipliedArray: %+v\n\n", alphaPremultipliedArray)
+
+				newImg.Set(x, y, rgba)
+			} else {
+				var randomInt1 = uint8(rand.Intn(255))
+				var randomInt2 = uint8(rand.Intn(255))
+				var randomInt3 = uint8(rand.Intn(255))
+
+				newImg.Set(x, y, color.RGBA{randomInt1, randomInt2, randomInt3, 255})
+			}
+		}
+	}
+
+	return newImg
+}
+
 func loadImage(filepath string) (image.Image, error) {
 	infile, err := os.Open(filepath)
 	if err != nil {
-		return nil, err
+		fmt.Fprintln(os.Stderr, err)
 	}
 	defer infile.Close()
+
 	img, _, err := image.Decode(infile)
 	if err != nil {
-		return nil, err
+		fmt.Fprintln(os.Stderr, err)
 	}
 	return img, nil
 }
 
 func main() {
 	var img, _ = loadImage("go.png")
-	var gray = rgbaToGray(img)
+	var newImg = rgbaToRandom(img)
 
 	f, _ := os.Create("new.png")
 	defer f.Close()
-	png.Encode(f, gray)
+	png.Encode(f, newImg)
 }
